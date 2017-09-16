b31ac3418a3aab12e97777bb42cbd562a6ac2be6
meg768 <meg768@users.noreply.github.com>
2017-09-16 20:13:00 +0200

    -

--- src/commands/colorize.js
+++ src/commands/colorize.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 
-var sprintf = require('yow/sprintf');
-var isObject = require('yow/is').isObject;
+var sprintf    = require('yow/sprintf');
+var isObject   = require('yow/is').isObject;
 var isFunction = require('yow/is').isFunction;
-var config = require('../scripts/config.js');
+var isString   = require('yow/is').isString;
+var config     = require('../scripts/config.js');
 
 
 var Module = new function() {
@@ -19,27 +20,56 @@ var Module = new function() {
 		args.option('offset',   {alias:'o', describe:'Offset on strip', default:0});
 		args.option('length',   {alias:'l', describe:'Length to colorize', default:1});
 		args.option('color',    {alias:'c', describe:'Color', default:"rgb(64,0,0)"});
+		args.option('text',     {alias:'t', describe:'Text strip to colorize', default:undefined});
 
 		args.wrap(null);
 	}
 
 
 	function run(argv) {
-		var Strip = require('../scripts/neopixel-strip.js');
-		var strip = new Strip({
-			address : config.i2c.address,
-			length  : config.strip.length
-		});
-
-		strip.initialize().then(function() {
-			strip.colorize({
-				offset : argv.offset,
-				length : argv.length,
-				color  : argv.color
+		try {
+			var Strip = require('../scripts/neopixel-strip.js');
+			var strip = new Strip({
+				address : config.i2c.address,
+				length  : config.strip.length
 			});
 
-		})
-//		console.log(layout.getLayout(argv.text));
+			strip.initialize().then(function() {
+
+				if (isString(argv.text)) {
+					var Layout = require('../scripts/layout.js');
+					var layout = new Layout();
+
+					var words = layout.getLayout(argv.text);
+
+					words.forEach(function(word) {
+						console.log(word);
+						strip.colorize({
+							offset : word.offset,
+							length : word.length,
+							color  : argv.color
+						});
+
+					});
+
+				}
+				else {
+					strip.colorize({
+						offset : argv.offset,
+						length : argv.length,
+						color  : argv.color
+					});
+
+
+				}
+
+
+			})
+
+		}
+		catch(error) {
+			console.log(error);
+		}
 
 	}
 
