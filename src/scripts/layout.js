383043933bc46eb3193c2496b52da3b08084113e
meg768 <meg768@users.noreply.github.com>
2017-09-18 16:52:47 +0200

    -

--- src/scripts/layout.js
+++ src/scripts/layout.js
@@ -96,7 +96,8 @@ module.exports = function() {
                 if (words.length == 1)
                     result.push(match);
                 else {
-                    match.next = findAllWords(words.slice(1), Math.min(match.index + match.text.length + 1, Math.floor(match.index / _columns) + _columns));
+                    //match.next = findAllWords(words.slice(1), Math.min(match.index + match.text.length + 1, Math.floor(match.index / _columns) + _columns));
+                    match.next = findAllWords(words.slice(1), match.index + match.text.length + 1);
 
                     if (match.next.length > 0)
                         result.push(match);
