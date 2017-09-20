e84480cb52ae5a1dddd108aac8d8996fbf796e14
meg768 <meg768@users.noreply.github.com>
2017-09-20 21:32:31 +0200

    -

--- src/commands/test.js
+++ src/commands/test.js
@@ -3,7 +3,6 @@
 var sprintf = require('yow/sprintf');
 var isObject = require('yow/is').isObject;
 var isFunction = require('yow/is').isFunction;
-var config = require('../scripts/config.js');
 
 
 var Module = new function() {
@@ -18,15 +17,6 @@ var Module = new function() {
 
 		args.option('text',       {alias:'t', describe:'Text to display', default:'FEM I HALV SEX'});
 
-/*
-		args.option('url',       {alias:'u', describe:'Socket IO url', default:config.service.url});
-		args.option('address',   {alias:'a', describe:'I2C bus address', default:config.i2c.address});
-		args.option('length',    {alias:'l', describe:'Neopixel strip length', default:config.strip.length});
-		args.option('segments',  {alias:'s', describe:'Number of segments in strip', default:config.strip.segments});
-		args.option('service',   {alias:'n', describe:'Service name', default:config.service.url});
-		args.option('interval',  {alias:'i', describe:'Refresh interval for clock in seconds', default:60});
-		args.option('debug',     {alias:'d', describe:'Debug, low lights', default:false});
-*/
 		args.wrap(null);
 
 		args.check(function(argv) {
@@ -36,10 +26,7 @@ var Module = new function() {
 
 
 	function run(argv) {
-		var Layout = require('../scripts/layout.js');
-		var layout = new Layout();
-
-		console.log(layout.getLayout(argv.text));
+		console.log(argv.text);
 
 	}
 
