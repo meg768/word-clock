1035cfaff0fd3bfd3ee1b62a02fd03e7a8aa6504
meg768 <meg768@users.noreply.github.com>
2017-09-20 20:32:10 +0200

    -

--- src/scripts/avanza-animation.js
+++ src/scripts/avanza-animation.js
@@ -149,7 +149,7 @@ module.exports = class extends Animation {
 
 			self.login().then(function() {
 				var provider = self.textProviders[self.textProviderIndex++ % self.textProviders.length];
-				return provider.bind(this)();
+				return provider.bind(self)();
 			})
 			.then(function(text) {
 				resolve(text);
