eccb57dfed703858764283bef37fd84d0f762c93
meg768 <meg768@users.noreply.github.com>
2017-09-22 10:48:42 +0200

    -

--- src/scripts/neopixel-strip.js
+++ src/scripts/neopixel-strip.js
@@ -147,9 +147,9 @@ module.exports = function NeopixelStrip(options) {
 				var g2 = (_pixels[i] & 0xFF) >> 8;
 				var b2 = (_pixels[i] & 0xFF);
 
-				var red   = parseInt(r1 + (step * (r2 - r1)) / numSteps);
-				var green = parseInt(g1 + (step * (g2 - g1)) / numSteps);
-				var blue  = parseInt(b1 + (step * (b2 - b1)) / numSteps);
+				var red   = (r1 + (step * (r2 - r1)) / numSteps);
+				var green = (g1 + (step * (g2 - g1)) / numSteps);
+				var blue  = (b1 + (step * (b2 - b1)) / numSteps);
 
 				display[i] = (red << 16) | (green << 8) | blue;
 				console.log(r2, g2, b2, red, green, blue,  (red << 16) | (green << 8) | blue);
