14d78caac8567f3463f675b4432aa938cade3bd9
meg768 <meg768@users.noreply.github.com>
2017-09-22 10:29:37 +0200

    -

--- src/scripts/neopixel-strip.js
+++ src/scripts/neopixel-strip.js
@@ -144,7 +144,7 @@ module.exports = function NeopixelStrip(options) {
 
 				display[i] = (red << 16) | (green << 8) | blue;
 			}
-			console.log('Rendering');
+			console.log('Rendering', i, _length);
 			_strip.render(display);
 		}
 
