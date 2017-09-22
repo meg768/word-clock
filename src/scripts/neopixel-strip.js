a4f9bf1a892dd3351df9a68d186557cab72ef156
meg768 <meg768@users.noreply.github.com>
2017-09-22 15:43:26 +0200

    -

--- src/scripts/neopixel-strip.js
+++ src/scripts/neopixel-strip.js
@@ -17,7 +17,7 @@ module.exports = function NeopixelStrip(options) {
 	var _length        = _width * _height;
 	var _strip         = require('rpi-ws281x-native');
 	var _pixels        = new Uint32Array(_length);
-	var tmp = new Uint32Array(_length);
+	//var tmp = new Uint32Array(_length);
 
 	_this.length = _length;
 	_this.width  = _width;
