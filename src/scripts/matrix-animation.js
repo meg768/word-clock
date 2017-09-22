f9f31d45cc49bb3b90d161fce01db6436be15dd3
meg768 <meg768@users.noreply.github.com>
2017-09-22 22:35:00 +0200

    -

--- src/scripts/matrix-animation.js
+++ src/scripts/matrix-animation.js
@@ -48,7 +48,7 @@ class Worm {
 
         self.length = self.height * 0.1 + self.height * 1.1 * random(100) / 100;
 		self.row    = -random(0, self.height * 2);
-        self.loops  = 1 + random(10);
+        self.loops  = random(5);
 		self.ticks  = 0;
 
 	}
