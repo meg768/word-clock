2bd93be25f2066623934c825995c406af52b0de7
meg768 <meg768@users.noreply.github.com>
2017-09-23 17:50:14 +0200

    -

--- src/commands/test.js
+++ src/commands/test.js
@@ -29,16 +29,16 @@ var Module = new function() {
 		var buttons = [
 			{pin: 19, name:'Button 1'},
 			{pin: 13, name:'Button 2'},
-			{pin:  6, name:'Button 3'},
+			{pin:  6, name:'Button 3'}
 		];
 
 		var Gpio = require('pigpio').Gpio;
 
 
 		buttons.forEach(function(button) {
-			var button = new Gpio(button.pin, {mode: Gpio.INPUT, pullUpDown: Gpio.PUD_DOWN, edge: Gpio.EITHER_EDGE});
+			var gpio = new Gpio(button.pin, {mode: Gpio.INPUT, pullUpDown: Gpio.PUD_DOWN, edge: Gpio.EITHER_EDGE});
 
-			button.on('interrupt', function (level) {
+			gpio.on('interrupt', function (level) {
 				console.log('Button', button.name, 'pressed', level);
 			});
 		});
