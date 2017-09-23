2ebcdd318045f9a7ffcc611f0fd58749706245a5
meg768 <meg768@users.noreply.github.com>
2017-09-23 16:50:23 +0200

    -

--- src/commands/test.js
+++ src/commands/test.js
@@ -4,9 +4,7 @@ var sprintf = require('yow/sprintf');
 var isObject = require('yow/is').isObject;
 var isFunction = require('yow/is').isFunction;
 var Color = require('color');
-var strip = require('rpi-ws281x-native');
-var neopixels = require('rpi-ws281x-native');
-var pixels =  new Uint32Array(169);
+var Gpio = require('pigpio').Gpio;
 
 var Module = new function() {
 
@@ -27,62 +25,13 @@ var Module = new function() {
 		});
 	}
 
-	function exitHandler(options, err) {
-		neopixels.reset();
-	    if (err) console.log(err.stack);
-	    if (options.exit) process.exit();
-	}
-
-	function fill(color) {
-		for (var i = 0; i < 169; i++) {
-			pixels[i] = color;
-		}
-		neopixels.render(pixels);
-	}
-
-	function setPixel(index, color) {
-		pixels[index] = color;
-	}
-
-	function foo() {
-
-		console.log('Running');
-		pixels[0] = Color().hsl(240, 100, 50).rgbNumber();
-		neopixels.render(pixels);
-
-		for (var i = 0; i < 169; i++) {
-			if (i > 0)
-				pixels[i - 1] = 0;
-			pixels[i] = Color().hsl(i, 100, 50).rgbNumber();
-			neopixels.render(pixels);
-		}
-
-		for (var i = 0; i < 25; i++)
-			fill(Color().hsl(0, 100, i).rgbNumber());
-/*
-		for (var i = 25; i >= 0; i--)
-			fill(Color().hsl(0, 100, i).rgbNumber());
-*/
-		neopixels.render(pixels);
-
-
-		//do something when app is closing
-		//process.on('exit', exitHandler.bind(null,{cleanup:true}));
-
-		//catches ctrl+c event
-		//process.on('SIGINT', exitHandler.bind(null, {exit:true}));
-
-		//catches uncaught exceptions
-		//process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
-
-
 	}
 	function run(argv) {
-		console.log('Initializing');
-		neopixels.init(169);
-		console.log('Waiting');
-		setTimeout(foo, 0);
+		var button = new Gpio(4, {mode: Gpio.INPUT, pullUpDown: Gpio.PUD_DOWN, edge: Gpio.EITHER_EDGE});
 
+		button.on('interrupt', function (level) {
+			led.digitalWrite(level);
+		});
 	}
 
 
--- src/scripts/neopixel-strip.js
+++ src/scripts/neopixel-strip.js
@@ -4,6 +4,22 @@ var isString = require('yow/is').isString;
 var isObject = require('yow/is').isObject;
 var Pixels   = require('./pixels.js');
 
+/*
+function exitHandler(options, err) {
+	neopixels.reset();
+	if (err) console.log(err.stack);
+	if (options.exit) process.exit();
+}
+*/
+//do something when app is closing
+//process.on('exit', exitHandler.bind(null,{cleanup:true}));
+
+//catches ctrl+c event
+//process.on('SIGINT', exitHandler.bind(null, {exit:true}));
+
+//catches uncaught exceptions
+//process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
+
 module.exports = function NeopixelStrip(options) {
 
 
