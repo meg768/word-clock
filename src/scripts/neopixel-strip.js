006a0837cd1488e96bb22ed28a4ad7699a340cd8
meg768 <meg768@users.noreply.github.com>
2017-09-25 19:33:25 +0200

    -

--- src/scripts/neopixel-strip.js
+++ src/scripts/neopixel-strip.js
@@ -112,6 +112,21 @@ module.exports = function NeopixelStrip(options) {
 	    }
 
 		_strip.setIndexMapping(map);
+
+		function exitHandler(options, err) {
+			_this.reset();
+			if (err) console.log(err.stack);
+			if (options.exit) process.exit();
+		}
+
+		//do something when app is closing
+		process.on('exit', exitHandler.bind(null,{cleanup:true}));
+
+		//catches ctrl+c event
+		process.on('SIGINT', exitHandler.bind(null, {exit:true}));
+
+		//catches uncaught exceptions
+		process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
 	}
 
 
