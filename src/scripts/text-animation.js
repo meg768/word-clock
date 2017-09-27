8d9c9a42f7d8fb0b310e86f333c22b9d26f7f9e0
meg768 <meg768@users.noreply.github.com>
2017-09-27 09:15:24 +0200

    -

--- src/scripts/text-animation.js
+++ src/scripts/text-animation.js
@@ -7,6 +7,39 @@ var Animation = require('./animation.js');
 var Pixels    = require('./pixels.js');
 var Layout    = require('./layout.js');
 
+
+class Star {
+
+    constructor(x, y, index, hue) {
+        this.x = x;
+        this.y = y;
+        this.luminance = 0;
+        this.index = index;
+        this.hue = hue;
+        this.loop = 0;
+        this.finished = false;
+    }
+
+    draw(pixels) {
+        pixels.setPixelHLS(this.x, this.y, this.hue, 100, this.luminance);
+    }
+
+
+    idle() {
+        if (this.index == this.loop) {
+            this.luminance = 50;
+        }
+        else {
+            this.luminance = this.luminance * 0.8;
+        }
+
+        if (this.loop >= this.index && this.luminance < 1)
+            this.finished = true;
+
+        this.loop++;
+    }
+}
+
 module.exports = class extends Animation {
 
 
@@ -30,25 +63,36 @@ module.exports = class extends Animation {
             console.log(options);
 
             var layout  = display.lookupLetters(options.text);
-            var color   = Color(options.color).rgbNumber();
+            var hue     = Color(options.color).hue;
             var letters = options.text.split('');
+            var stars   = [];
 
             console.log('letters', letters);
 
             pixels.fill(0);
             strip.render(pixels.getPixels(), {fadeIn:20});
 
-            letters.forEach((letter) => {
+            for (var index = 0; index < letters.length; index++) {
                 var position = random(layout[letter]);
+                var star = new Star(position.x, position.y, hue, index);
 
-                console.log(position);
-                pixels.setPixel(position.x, position.y, color);
-                strip.render(pixels.getPixels(), {fadeIn:4});
-                pixels.setPixel(position.x, position.y, 0);
-            });
+                stars.push(position);
+            }
 
-            pixels.clear();
-            strip.render(pixels.getPixels(), {fadeIn:20});
+            for (;;) {
+                for (var index = 0; index < stars.length; index++) {
+                    stars[index].idle();
+                    stars[index].draw(pixels);
+                }
+                strip.render(pixels.getPixels());
+
+                if (stars[stars.length - 1].finished)
+                    break;
+            }
+
+
+//            pixels.clear();
+//            strip.render(pixels.getPixels(), {fadeIn:20});
 
             resolve();
         });
