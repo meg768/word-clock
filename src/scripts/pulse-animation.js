2dc5d2831abed4d1323c036604009d6da953620e
meg768 <meg768@users.noreply.github.com>
2017-11-04 18:31:10 +0100

    -

--- src/scripts/pulse-animation.js
+++ src/scripts/pulse-animation.js
@@ -50,7 +50,9 @@ module.exports = class extends Animation {
             this.pixels.fill((this.tick % 2) == 0 ? this.color : 0);
             this.strip.render(this.pixels.getPixels(), {fadeIn:this.options.delay});
 
+            this.tick = 0;
         }
+        
         catch(error) {
             console.log(error);
         }
