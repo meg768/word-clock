9d912d14572c524ebfd7ae7ecbcedafbc8142127
meg768 <meg768@users.noreply.github.com>
2017-11-04 13:43:34 +0100

    -

---
+++ src/scripts/color-animation.js
@@ -0,0 +1,37 @@
+var sprintf    = require('yow/sprintf');
+var random     = require('yow/random');
+var Colors     = require('color-convert');
+var isArray    = require('yow/is').isArray;
+var isString   = require('yow/is').isString;
+var Animation  = require('./animation.js');
+
+
+module.exports = class extends Animation {
+
+
+	constructor(strip, options) {
+		super(strip, Object.assign({renderFrequency:60000, color:'blue'}, options));
+
+		this.name  = 'Color animation';
+		this.color = 'blue';
+
+		try {
+			this.color = Color(this.options.color).rgbNumber();
+		}
+		catch(error) {
+			this.color = 'red';
+		}
+	}
+
+
+
+	render() {
+		this.pixels.fill(this.color);
+		this.strip.render(this.pixels.getPixels(), {fadeIn:10});
+
+    }
+
+
+
+
+};
