94d9390d7ed74d9110ac6a14886c5e784afdbf2f
meg768 <meg768@users.noreply.github.com>
2017-11-05 09:35:18 +0100

    -

--- src/commands/server.js
+++ src/commands/server.js
@@ -52,7 +52,7 @@ var Module = new function() {
 		registerService().then(function() {
 
 //			var animations       = [PulseAnimation, ClockAnimation, IndexAnimation, CommodityAnimation, CurrencyAnimation];
-			var animations       = [PulseAnimation, ColorAnimation, ClockAnimation]; //, IndexAnimation, CommodityAnimation, CurrencyAnimation];
+			var animations       = [ClockAnimation, IndexAnimation, CommodityAnimation, CurrencyAnimation];
 			var upperButton      = new Button(6);
 			var lowerButton      = new Button(13);
 			var strip            = new Strip({width:13, height:13});
