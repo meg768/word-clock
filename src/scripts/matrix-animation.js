23c2f97c326bb4bb6a53ce83d605468077ec8df1
meg768 <meg768@users.noreply.github.com>
2018-03-07 17:46:33 +0100

    -

--- src/scripts/matrix-animation.js
+++ src/scripts/matrix-animation.js
@@ -41,7 +41,7 @@ class Worm {
 
         self.length = Math.floor(self.height * 0.1 + self.height * 0.9 * random(100) / 100);
 		self.row    = -self.length;
-        self.loops  = random([0, 0, 0, 0, 1, 1, 1, 2, 2, 3, 4]);
+        self.loops  = 2 * random([1, 1, 1, 1, 2, 2, 2, 3, 3, 4, 5]);
 		self.ticks  = 0;
         self.hue    = Math.floor(360 * (((now.getHours() % 12) * 60) + now.getMinutes()) / (12 * 60));
 	}
