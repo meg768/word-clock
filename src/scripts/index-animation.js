f9b0329602a5e63b30b57f53ce96e467551e1b90
meg768 <meg768@users.noreply.github.com>
2019-05-30 17:50:32 +0200

    -

--- src/scripts/index-animation.js
+++ src/scripts/index-animation.js
@@ -1,5 +1,6 @@
 var Animation = require('./yahoo-animation.js');
 var cached = require('./cached.js');
+var Yahoo = require('./yahoo-finance.js')
 
 var fetchIndices = function(yahoo) {
 
@@ -21,13 +22,33 @@ var fetchIndices = function(yahoo) {
 
 var getIndices = cached(fetchIndices, 60000);
 
-module.exports = class extends Animation {
+module.exports = class Module extends Animation {
 
 	constructor(pixels, options) {
 		super(pixels, options);
 
 		this.name = 'Yahoo Index Animation';
 	}
+
+	static fetchQuotes() {
+		var symbols = [
+			{name:'OMX', symbol:'^OMX'},
+			{name:'NASDAQ', symbol:'^IXIC'},
+			{name:'DAX', symbol:'^GDAXI'},
+			{name:'DOWJONES', symbol:'^DJI'},
+			{name:'HANGSENG', symbol:'^HSI'},
+			{name:'USA', symbol:'^GSPC'},
+			{name:'UK', symbol:'^FTSE'},
+			{name:'BRIC', symbol:'^BSESN'},
+			{name:'NIKKEI', symbol:'^N225'}
+	
+		];
+
+		var yahoo = new Yahoo();
+		return yahoo.fetchQuotes(symbols);
+	}
+
+	static foo = Module.fetchQuotes;
 
 	getQuotes() {
 		return getIndices(this.yahoo);
