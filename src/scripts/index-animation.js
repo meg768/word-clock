8355c3c297a2ce26943c288716d5046507ab7d9f
meg768 <meg768@users.noreply.github.com>
2019-05-30 18:05:15 +0200

    -

--- src/scripts/index-animation.js
+++ src/scripts/index-animation.js
@@ -2,8 +2,7 @@ var Animation = require('./yahoo-animation.js');
 var cached = require('./cached.js');
 var Yahoo = require('./yahoo-finance.js')
 
-var fetchIndices = function(yahoo) {
-
+var fetchQuotes = cached(() => {
 	var symbols = [
 		{name:'OMX', symbol:'^OMX'},
 		{name:'NASDAQ', symbol:'^IXIC'},
@@ -20,9 +19,9 @@ var fetchIndices = function(yahoo) {
 	var yahoo = new Yahoo();
 
 	return yahoo.fetchQuotes(symbols);
-}
 
-var getIndices = cached(fetchIndices, 60000);
+}, 60000);
+
 
 module.exports = class Module extends Animation {
 
@@ -32,28 +31,9 @@ module.exports = class Module extends Animation {
 		this.name = 'Yahoo Index Animation';
 	}
 
-	static fetchQuotes() {
-		var symbols = [
-			{name:'OMX', symbol:'^OMX'},
-			{name:'NASDAQ', symbol:'^IXIC'},
-			{name:'DAX', symbol:'^GDAXI'},
-			{name:'DOWJONES', symbol:'^DJI'},
-			{name:'HANGSENG', symbol:'^HSI'},
-			{name:'USA', symbol:'^GSPC'},
-			{name:'UK', symbol:'^FTSE'},
-			{name:'BRIC', symbol:'^BSESN'},
-			{name:'NIKKEI', symbol:'^N225'}
-	
-		];
-
-		var yahoo = new Yahoo();
-		return yahoo.fetchQuotes(symbols);
-	}
-
-	static foo = 4;
 
 	getQuotes() {
-		return getIndices(this.yahoo);
+		return fetchQuotes();
 		return ([
 			{name:'OMX', symbol:'^OMX'},
 			{name:'NASDAQ', symbol:'^IXIC'},
