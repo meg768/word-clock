36b90a86c134e4cf032a2a073cde888543a3abc8
meg768 <meg768@users.noreply.github.com>
2019-07-26 18:51:02 +0200

    -

--- src/commands/rain.js
+++ src/commands/rain.js
@@ -19,7 +19,7 @@ var Module = new function() {
 
 
 	function run(argv) {
-		var p = new Neopixels.Pixels();
+		var p = new Neopixels();
 		console.log('p', p);
 		var animation = new Animation({pixels: p, duration:-1, priority:'!', debug:true});
 		return animation.run();
