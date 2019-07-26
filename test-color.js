d1e9837aaf11945ea353a4f1481c36c1ab3082e4
meg768 <meg768@users.noreply.github.com>
2019-07-26 16:58:14 +0200

    -

--- test-color.js
+++ test-color.js
@@ -10,8 +10,10 @@ var App = function() {
 
 		var colorHSL = Color({h:0, s:100, l:50});
 		var colorRGB = Color({r:255, g:0, b:0});
+		var colorText = Color('red');
 		console.log(colorHSL, colorHSL.rgbNumber());
 		console.log(colorRGB, colorRGB.rgbNumber());
+		console.log(colorText, colorText.rgbNumber());
 	};
 
 	run();
