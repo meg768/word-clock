88d6da606bda54061826de31e903f9e70ba9c8dd
meg768 <meg768@users.noreply.github.com>
2019-07-26 18:28:18 +0200

    -

--- src/commands/clock.js
+++ src/commands/clock.js
@@ -19,13 +19,8 @@ var Module = new function() {
 
 
 	function run(argv) {
-
-
 		var animation = new ClockAnimation({pixels: new Neopixels.Pixels(), duration:-1, priority:'!', debug:true});
-
 		return animation.run();
-
-
 	}
 
 	module.exports.command  = 'clock [options]';
