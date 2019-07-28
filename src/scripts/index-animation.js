36155a5719bea377e00752a259d52365e9be6546
meg768 <meg768@users.noreply.github.com>
2019-07-28 21:02:03 +0200

    -

--- src/scripts/index-animation.js
+++ src/scripts/index-animation.js
@@ -2,8 +2,8 @@ var Animation = require('./word-animation.js');
 var yahoo = require('./yahoo-finance.js')
 
 var quotes = [
-	{name:'OMX', symbol:'^OMX'},
-	{name:'NASDAQ', symbol:'^IXIC'},
+	{name:'OMX', symbol:'^OMX', change:0.5},
+	{name:'NASDAQ', symbol:'^IXIC', change:-0.5},
 	{name:'DAX', symbol:'^GDAXI'},
 	{name:'DOWJONES', symbol:'^DJI'},
 	{name:'HANGSENG', symbol:'^HSI'},
@@ -27,7 +27,27 @@ module.exports = class Module extends Animation {
 		var words = [];
 
 		quotes.forEach((item) => {
-			words.push({word:item.name, color:'red'});
+			var color = Color.rgb(64, 64, 64);
+
+			if (item.change != undefined) {
+				var change     = Math.max(-2, Math.min(2, item.change));
+				var hue        = change >= 0 ? 240 : 0;
+				var saturation = 100;
+				var luminance  = 10 + (Math.abs(change) / 2) * 40;
+	
+				if (Math.abs(item.change) > 2)
+					luminance = 60;
+	
+				if (Math.abs(item.change) > 2.5)
+					luminance = 65;
+	
+				if (Math.abs(item.change) > 3)
+					luminance = 70;
+	
+				color = Color.hsl(hue, saturation, luminance);
+			}
+
+			words.push({word:item.name, color:color});
 		});
 
 		return words;
