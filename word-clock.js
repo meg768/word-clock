951dc7f9df5f334ade480fd497c72b105bdd0e64
meg768 <meg768@users.noreply.github.com>
2019-07-28 20:20:06 +0200

    -

--- word-clock.js
+++ word-clock.js
@@ -24,6 +24,7 @@ var App = function() {
 			args.command(require('./src/commands/boring.js'));
 			args.command(require('./src/commands/word.js'));
 			args.command(require('./src/commands/weather.js'));
+			args.command(require('./src/commands/index.js'));
 
 			args.wrap(null);
 			args.demand(1);
