91c27775b9274c4c2e94eb7a77f709a5e33cbe9b
meg768 <meg768@users.noreply.github.com>
2019-07-30 23:51:23 +0200

    -

--- src/scripts/quotes.js
+++ src/scripts/quotes.js
@@ -4,6 +4,7 @@ var debug = require('./debug.js');
 var schedule = require('node-schedule');
 
 var quotes = {};
+var data = {};
 
 module.exports = class extends Events {
 
@@ -12,8 +13,8 @@ module.exports = class extends Events {
 		this.quotes = quotes;
 		this.jobs = [];
 	}
-
-	subscribe(symbols) {
+/*
+	subscribeX(id, fn) {
 
 		var job = schedule.scheduleJob({second:0}, () => {
 			var params = {};
@@ -23,15 +24,55 @@ module.exports = class extends Events {
 	
 			debug('Fetching quotes for symbols', symbols);
 	
+			fn.apply(this, arguments).then((reply) => {
+				data[id] = reply;
+				this.emit('data', this.reply);
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+		});
+
+		this.jobs.push(job);
+	}
+*/
+
+	fetchQuotes(symbols) {
+
+		return new Promise((resolve, reject) => {
+			var params = {};
+
+			params.symbols = symbols;
+			params.modules = ['price'];
+	
+			debug('Fetching quotes for symbols', symbols);
+	
 			yahoo.quote(params).then((data) => {
 	
+				var quotes = {};
+
 				symbols.forEach((symbol) => {
 					var change = data[symbol].price.regularMarketChangePercent;
 					var price = data[symbol].price.regularMarketPrice;
 	
-					this.quotes[symbol] = {change:change, price:price};
+					quotes[symbol] = {change:change, price:price};
 				});
+	
+				resolve(quotes);
+			})
+			.catch((error) => {
+				reject(error);
+			});
+	
+		});
 
+	}
+	subscribe(symbols) {
+
+		var job = schedule.scheduleJob({second:0}, () => {
+
+			this.fetchQuotes(symbols).then((quotes) => {
+				this.quotes = {...this.quotes, quotes};				
 				this.emit('quotes', this.quotes);
 			})
 			.catch((error) => {
