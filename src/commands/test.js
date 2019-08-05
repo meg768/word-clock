65c90b11b123c8cf8fb180b3e527d765dd4ecc02
meg768 <meg768@users.noreply.github.com>
2019-08-05 19:17:21 +0200

    -

--- src/commands/test.js
+++ src/commands/test.js
@@ -5,7 +5,7 @@ var WordAnimation = require('../scripts/word-animation.js');
 var debug = require('../scripts/debug.js');
 var Clock = require('../scripts/clock.js');
 
-module.exports = class extends WordAnimation {
+class Animation extends WordAnimation {
 
     constructor(options) {
 
@@ -46,7 +46,7 @@ var Module = new function() {
 	}
 
 	function run(argv) {
-		var animation = new Animation({pixels: new Neopixels(), duration:-1, priority:'!', debug:true});
+		var animation = new Animation({pixels: new Neopixels(), duration:-1, priority:'!', debug:debug});
 		return animation.run();
 
 
