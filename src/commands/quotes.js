85f08e6022e4072d2eadc89fcdf2ea2ec65d1260
meg768 <meg768@users.noreply.github.com>
2022-02-01 18:16:33 +0100

    -

--- src/commands/quotes.js
+++ src/commands/quotes.js
@@ -19,15 +19,7 @@ var Module = new function() {
 		var Quotes = require('../scripts/quotes.js');
 
 		var symbols = [
-			{name:'OMX', symbol:'^OMX'},
-			{name:'NASDAQ', symbol:'^IXIC'},
-			{name:'DAX', symbol:'^GDAXI'},
-			{name:'DOWJONES', symbol:'^DJI'},
-			{name:'HANGSENG', symbol:'^HSI'},
-			{name:'USA', symbol:'^GSPC'},
-			{name:'UK', symbol:'^FTSE'},
-			{name:'BRIC', symbol:'^BSESN'},
-			{name:'NIKKEI', symbol:'^N225'}
+			{name:'OMX', symbol:'^OMX'}
 		];
 		
 		var quotes = new Quotes(symbols);
