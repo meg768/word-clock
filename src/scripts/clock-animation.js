a65972e4999c9c3dcf04db7b69dec9481e37bc62
meg768 <magnus@egelberg.se>
2025-09-24 19:53:57 +0200

    -

--- src/scripts/clock-animation.js
+++ src/scripts/clock-animation.js
@@ -24,7 +24,7 @@ module.exports = class extends WordAnimation {
 
         // Add the day of the week
 
-        words.push({ word: day, color: 'silver' });
+        words.push({ word: day, color: 'gray' });
 
         return words;
     }
