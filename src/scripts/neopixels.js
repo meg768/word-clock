58f163647e70bd7463a6e80defe8df24ba19c3d2
meg768 <magnus@egelberg.se>
2025-09-26 14:28:02 +0200

    -

--- src/scripts/neopixels.js
+++ src/scripts/neopixels.js
@@ -1,4 +1,36 @@
 var Neopixels = require('rpi-neopixels');
+var debug = require('./debug.js');
+
+function configure() {
+	function cleanup() {
+		debug('Cleaning up...');
+		var pixels = new Neopixels();
+
+		pixels.fill('black');
+		pixels.render();
+
+		process.exit();
+	}
+
+	var stripType = 'grb';
+	var width = 13;
+	var height = 13;
+	var map = 'alternating-matrix';
+
+	Neopixels.configure({ debug: debug, map: map, width: width, height: height, stripType: stripType });
+
+	process.on('SIGUSR1', cleanup);
+	process.on('SIGUSR2', cleanup);
+	process.on('SIGINT', cleanup);
+	process.on('SIGTERM', cleanup);
+}
+
+configure();
+
+module.exports = Neopixels;
+
+/*
+var Neopixels = require('rpi-neopixels');
 
 
 let config = {
@@ -15,3 +47,4 @@ let config = {
 };
 
 module.exports = new Neopixels(config);
+*/
