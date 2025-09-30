ccdb0efef480677a0504808411aca95291e6b53e
meg768 <magnus@egelberg.se>
2025-09-30 22:16:16 +0200

    -

--- src/scripts/animations.js
+++ src/scripts/animations.js
@@ -13,8 +13,8 @@ module.exports = class extends Events {
 		this.iterations = iterations;
 		this.renderFrequency = renderFrequency;
 		this.renderTime = undefined;
-        console.log('Debug is', debug);
-        console.log('Debug is', typeof debug === 'function');
+        console.log('Debug value', debug);
+        console.log('Debug is function', typeof debug === 'function');
 		this.debug = typeof debug === 'function' ? debug : debug ? console.log : () => {};
 	}
 
