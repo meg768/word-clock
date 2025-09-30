2a75578957f8661e1b07c79cb423833fb43cde13
meg768 <magnus@egelberg.se>
2025-09-30 20:18:46 +0200

    -

--- src/scripts/quote-animation.js
+++ src/scripts/quote-animation.js
@@ -6,14 +6,13 @@ var debug = require('./debug.js');
 var cache = {};
 
 module.exports = class Module extends WordAnimation {
-
 	constructor(options) {
-		var {symbols, ...options} = options;
+		var { symbols, ...options } = options;
 
-		super({name:'Yahoo Animation', renderFrequency: 60 * 1000, ...options});
+		super({ name: 'Yahoo Animation', renderFrequency: 60 * 1000, ...options });
 
 		if (cache[this.name] == undefined) {
-			var quotes = cache[this.name] = new Quotes(symbols);
+			var quotes = (cache[this.name] = new Quotes(symbols));
 
 			quotes.on('quotes', () => {
 				this.render();
@@ -24,32 +23,27 @@ module.exports = class Module extends WordAnimation {
 		this.symbols = symbols;
 	}
 
-
-
 	getWords() {
 		var words = [];
 		var quotes = this.quotes.quotes;
 		var symbols = this.symbols;
 
-		symbols.forEach((symbol) => {
+		symbols.forEach(symbol => {
 			var color = Color.rgb(32, 32, 32);
 			var quote = quotes[symbol.symbol];
 
 			if (quote != undefined && quote.change != undefined) {
-
-				var change     = Math.max(-2, Math.min(2, quote.change));
-				var hue        = change >= 0 ? 240 : 0;
+				var change = Math.max(-2, Math.min(2, quote.change));
+				var hue = quote.change >= 0 ? 240 : 0;
 				var saturation = 100;
-				var luminance  = 20 + (Math.abs(change) / 2) * 30;
-		
+				var luminance = 10; //Math.abs(change) / 2) * 30;
+
 				color = Color.hsl(hue, saturation, luminance);
 			}
 
-			words.push({word:symbol.name, color:color});
+			words.push({ word: symbol.name, color: color });
 		});
 
 		return words;
-		
 	}
-
 };
