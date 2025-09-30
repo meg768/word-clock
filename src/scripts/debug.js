d3852edd92c555526816e2430cde9cdcb082b931
meg768 <magnus@egelberg.se>
2025-09-30 22:39:50 +0200

    -

--- src/scripts/debug.js
+++ src/scripts/debug.js
@@ -1,37 +1,13 @@
 var sprintf = require('yow/sprintf');
 var debugMode = process.env.DEBUG == undefined ? false : parseInt(process.env.DEBUG) != 0;
 
-function debugPrint() {
-	var date = new Date();
-	var args = Array.prototype.slice.call(arguments);
-
-	args.unshift(sprintf('%04d-%02d-%02d %02d:%02d.%02d:', date.getFullYear(), date.getMonth() + 1, date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds()));
-
-	console.log.apply(this, args);
-}
-
-function debugNoop() {
-
-}
-	module.exports = debugPrint;		
-
-/*
-if (debugMode) {
-	module.exports = debugPrint;		
-}
-else {
-	module.exports = debugNoop;
-}
-
-*/
-/*
 module.exports = function () {
-		return debugPrint;
 	if (debugMode) {
-		return debugPrint;
-	}
-	else {
-		return debugNoop;
+		var date = new Date();
+		var args = Array.prototype.slice.call(arguments);
+
+		args.unshift(sprintf('%04d-%02d-%02d %02d:%02d.%02d:', date.getFullYear(), date.getMonth() + 1, date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds()));
+
+		console.log.apply(this, args);
 	}
 };
-*/
