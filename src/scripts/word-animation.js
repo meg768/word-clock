876d437df0a386904d638c809b8ad9b1e1c765ac
meg768 <magnus@egelberg.se>
2025-09-30 22:33:00 +0200

    -

--- src/scripts/word-animation.js
+++ src/scripts/word-animation.js
@@ -1,68 +1,67 @@
-
 var Animation = require('./animations.js');
 var Layout = require('./layout.js');
 var Color = require('color');
 var sprintf = require('yow/sprintf');
+var debug = require('./debug.js');
 
 var maxMilliAmpere = 0;
 
 module.exports = class extends Animation {
+	constructor(options) {
+		var { pixels, ...options } = options;
 
+		super({ renderFrequency: 10 * 1000, name: 'Word Animation', ...options });
 
-    constructor(options) {
-        var {pixels, ...options} = options;
-        var debug = require('./debug.js');
-      
-        super({renderFrequency: 10 * 1000, debug:false, name:'Word Animation', ...options});
-        
-        this.pixels = pixels;
-    }
+		this.pixels = pixels;
+	}
 
-    getWords() {
-        return [
-            {word:'FEM', color:'red'},
-            {word:'I', color:'red'},
-            {word:'SEX', color:'blue'}
-        ];
-    }
+	getWords() {
+		return [
+			{ word: 'FEM', color: 'red' },
+			{ word: 'I', color: 'red' },
+			{ word: 'SEX', color: 'blue' }
+		];
+	}
 
-    render() {
-        var layout = new Layout();
-        var words = this.getWords();
+	render() {
+		var layout = new Layout();
+		var words = this.getWords();
 
-        var items = [];
-        var text = [];
-        var milliAmpere = 0;
-        
-        words.forEach((item) => {
-            items.push(item.word);
-            text.push(item.word);    
-        });
+		var items = [];
+		var text = [];
+		var milliAmpere = 0;
 
-        items = layout.lookup(items);
+		words.forEach(item => {
+			items.push(item.word);
+			text.push(item.word);
+		});
 
-        this.pixels.clear();
+		items = layout.lookup(items);
 
-        items.forEach((item, index) => {
-            var color = Color(words[index].color).rgbNumber();
-            var power = 0;
+		this.pixels.clear();
 
-            Color(color).rgb().array().forEach((value) => {
-                power += (20 * value) / 255;
-            });
+		items.forEach((item, index) => {
+			var color = Color(words[index].color).rgbNumber();
+			var power = 0;
 
-            for (var i = 0; i < item.word.length; i++) {
-                this.pixels.setPixel(item.x + i, item.y, color);
-            }
+			Color(color)
+				.rgb()
+				.array()
+				.forEach(value => {
+					power += (20 * value) / 255;
+				});
 
-            milliAmpere += item.word.length * power;
-        });
+			for (var i = 0; i < item.word.length; i++) {
+				this.pixels.setPixel(item.x + i, item.y, color);
+			}
 
-        maxMilliAmpere = Math.max(maxMilliAmpere, milliAmpere);
+			milliAmpere += item.word.length * power;
+		});
 
-        this.debug(sprintf('"%s" (%d mA/%d mA)', text.join(' '), milliAmpere, maxMilliAmpere));
+		maxMilliAmpere = Math.max(maxMilliAmpere, milliAmpere);
 
-        this.pixels.render();
-    }
+		debug(sprintf('"%s" (%d mA/%d mA)', text.join(' '), milliAmpere, maxMilliAmpere));
 
-}
+		this.pixels.render();
+	}
+};
