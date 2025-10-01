eaa4c07bbb6254c67f8d12733c925104d7fc1152
meg768 <magnus@egelberg.se>
2025-10-01 20:03:33 +0200

    -

--- reload.sh
+++ reload.sh
@@ -6,8 +6,5 @@ cd "$(dirname "$0")"
 echo "[Word-Clock] Pulling latest code..."
 git pull --ff-only
 
-echo "[Word-Clock] Reloading PM2 processes..."
-pm2 reload all
-
 echo "[Word-Clock] Rebooting system..."
 sudo -n /sbin/reboot 2>/dev/null || sudo /sbin/reboot
