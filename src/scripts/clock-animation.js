973faae799778fed202e119b3e00fd06f2681d58
meg768 <magnus@egelberg.se>
2025-10-03 17:23:54 +0200

    -

--- src/scripts/clock-animation.js
+++ src/scripts/clock-animation.js
@@ -1,33 +1,38 @@
-
 var Clock = require('./clock.js');
 var WordAnimation = require('./word-animation.js');
 
 module.exports = class extends WordAnimation {
+	constructor(options) {
+		super({ name: 'Clock Animation', renderFrequency: 15 * 1000, ...options });
+	}
 
-    constructor(options) {
-        super({name:'Clock Animation', renderFrequency: 15 * 1000, ...options});
-    }
+	getWords() {
+		var clock = new Clock(new Date());
+		var words = [];
+		var color = clock.getColor();
+		var time = clock.getTime();
+		var day = clock.getDay();
 
-    getWords() {
-        var clock = new Clock(new Date());
-        var words = [];
-        var color = clock.getColor();
-        var time  = clock.getTime();
-        var day   = clock.getDay();
+		time.split(' ').forEach(word => {
+			words.push({ word: word, color: color });
+		});
 
-        time.split(' ').forEach((word) => {
-            words.push({word:word, color:color});
-        });
+		// Hack - make the day of week light the last row
+		words.push({ word: 'NIKK', color: 'black' });
 
-        // Hack - make the day of week light the last row
-        words.push({ word: 'NIKK', color: 'black' });
+		// Add the day of the week
 
-        // Add the day of the week
+		switch (day) {
+			case 'SÖ':
+			case 'LÖ': {
+				words.push({ word: day, color: 'red' });
+				break;
+			}
+			default: {
+				words.push({ word: day, color: 'blue' });
+			}
+		}
 
-        words.push({ word: day, color: 'dimgray' });
-
-        return words;
-    }
-
-}
-
+		return words;
+	}
+};
