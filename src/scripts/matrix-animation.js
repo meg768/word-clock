f64ef65600f58a09e53d8bce0d24a0579328cb67
meg768 <magnus@egelberg.se>
2025-10-13 18:04:42 +0200

    -

--- src/scripts/matrix-animation.js
+++ src/scripts/matrix-animation.js
@@ -75,7 +75,7 @@ module.exports = class extends Animation {
         super({name:'Matrix Animation', ...options});
 
         this.pixels = pixels;
-        this.delay  = delay || 10;
+        this.delay  = delay || 15;
         this.worms  = [];
 
     }
