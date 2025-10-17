f11f3cfcbf01050c1a475574658554790d0ad216
meg768 <magnus@egelberg.se>
2025-10-17 21:34:49 +0200

    -

--- src/scripts/neopixels.js
+++ src/scripts/neopixels.js
@@ -16,3 +16,23 @@ function configure() {
 configure();
 
 module.exports = Neopixels;
+
+/*
+var Neopixels = require('rpi-neopixels');
+
+
+let config = {
+	debug: true,
+	width: 13,
+	height: 13,
+	map: 'serpentine',
+	dma: 10,
+	freq: 800000,
+	gpio: 18,
+	invert: false,
+	brightness: 255,
+	stripType: 'ws2811-grb'
+};
+
+module.exports = new Neopixels(config);
+*/
