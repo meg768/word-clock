b61f429ab8565abca9c6d167a3c66247ccaa1f87
meg768 <magnus@egelberg.se>
2025-11-13 18:04:20 +0100

    -

--- src/scripts/animation.js
+++ src/scripts/animation.js
@@ -29,7 +29,7 @@ module.exports = class extends Events {
     }
 
     start() {
-        this.debug('Starting animation', this.name);
+        this.debug('Starting animationX', this.name);
 
         return new Promise((resolve, reject) => {
 
